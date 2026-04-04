925e2d33a64c0d76b8c0a3b81ebdb21f8b588d75
Joshua Brose <joshuabrose39@gmail.com>
2026-04-04 14:25:28 -0500

    Stack part-time filter label above checkbox

--- frontend/src/components/PartTimeFilter.js
+++ frontend/src/components/PartTimeFilter.js
@@ -3,13 +3,13 @@ import React from 'react';
 export default function PartTimeFilter({ checked, setChecked }) {
   return (
     <div className="filter-section part-time-filter">
-      <label className="part-time-filter-label">
+      <div className="part-time-filter-label">Include Part Time Educators?</div>
+      <label className="part-time-filter-checkbox-row">
         <input
           type="checkbox"
           checked={checked}
           onChange={(event) => setChecked(event.target.checked)}
         />
-        <span>Include Part Time Educators?</span>
       </label>
     </div>
   );
